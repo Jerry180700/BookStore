83d5bdff8214215a5cfe2a69ba9df2536379cf1e
Gerardo Gutierrez Velasco <g.gutierrez@lainter.edu.mx>
2024-09-03 15:20:32 -0600

    se completa la configuración de devise en application.html.erb y en development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'https://book-store-jerry180700-a0bfb225fca9.herokuapp.com/', port: 3000 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
