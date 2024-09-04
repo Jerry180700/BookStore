04bab7f561e5e247481e561e64ddd63d72d5e9fc
Manuel <109311578+mpinolivon1@users.noreply.github.com>
2024-09-04 14:31:51 -0500

    Revert "se agregaron las validaciones en order.rb"

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :book
-
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
-  validates :book_id, presence: true
-  validates :user_id, presence: true
-  validate  :book_must_have_sufficient_stock
-
-  def book_must_have_sufficient_stock
-    return if book.nil? || quantity <= book.avaliable_quantity
-
-    errors.add(:quantity, "Excede el stock")
-  end
 end
