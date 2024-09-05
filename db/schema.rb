53562d1acad908e61c2eefc4166ae1dc16ff8589
Gerardo Gutierrez Velasco <g.gutierrez@lainter.edu.mx>
2024-09-05 17:34:59 -0600

    se arregla el flujo de trabajo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_05_215957) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_05_223809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_05_215957) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img_url"
   end
 
   create_table "orders", force: :cascade do |t|
